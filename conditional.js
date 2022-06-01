9310444a2672e374d1c4622e1e97527a8d69b970
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-01 11:49:26 +0600

    first commit

---
+++ conditional.js
@@ -0,0 +1,94 @@
+// // var isKhabarReady = true;
+// //zodi food ready hoy taholoe ami khabo 
+
+// // // if food is ready == true;
+// // // (condition) {
+
+// // // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // if (isKhabarReady == true) {
+// //     console.log("i will eat now");
+// // }
+
+
+
+
+// // var iPhone = 70000;
+
+// // var mYbujet = 190050;
+
+// // if (iPhone < mYbujet) {
+
+// //     console.log("ami mobile ta kinbo,,kine futani marbo!!")
+
+// // }
+// // hehe  output____________
+
+
+
+
+// var iPhone = 70000;
+// var mYbujet = 9000;
+// if (iPhone > mYbujet) {
+//     console.log("My redmi is allways best ")
+// }
+
+
+
+
+
+
+// if (condition) {
+
+
+// }
+
+// else{
+
+
+// }
+
+
+
+
+var chickenPrice = 180;
+var myMoney = 850;
+
+if (chickenPrice < myMoney) {
+    console.log(" yesss!! ami murgir ran khamu are haddi cabamu....!!  ")
+}
+else {
+    console.log("alu vorta is better then chicken hihihih!!!!!")
+}
+
+
+
+
+
+var chickenPrice = 180;
+var myMoney = 50;
+
+if (chickenPrice < myMoney) {
+    console.log(" yesss!! ami murgir ran khamu are haddi cabamu....!!  ")
+}
+else {
+    console.log("alu vorta is better then chicken hihihih!!!!!")
+}
+
+
+
+
+
+
